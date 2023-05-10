f4c4616556b7868c3623caa0379cdf5b4016942a
Erickson <espejo.erickson@gmail.com>
2023-05-10 16:20:08 -0500

    mis archivos web

---
+++ index.php
@@ -0,0 +1,38 @@
+<?php
+    include_once 'templates/header/header.php';
+?>
+
+<?php
+    include_once 'templates/content/slider/slider.php';
+?>
+
+<?php 
+    include_once 'templates/content/services/services.php';
+?>
+
+<?php 
+    include_once 'templates/content/plans/plans.php'
+?>
+<?php 
+    include_once 'templates/content/calltoaction/calltoactions.php'
+?>
+<?php 
+    include_once 'templates/content/testimonials/testimonials.php'
+?>
+<?php 
+    include_once 'templates/content/contactus/contactus.php'
+?>
+<?php /*
+    include_once 'templates/content/slider/slider.php'
+?>
+<?php
+    include_once 'templates/content/slider/slider.php' */
+?>
+
+
+
+
+
+<?php
+    include_once 'templates/footer/footer.php'
+?>
---
+++ templates/content/testimonials/testimonials.php
@@ -0,0 +1,64 @@
+<section class="testimonials" id="testimonials">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-12">
+          <div class="section-heading">
+            <h6>Testimonials</h6>
+            <h4>What They <em>Think</em></h4>
+          </div>
+        </div>
+        <div class="col-lg-12">
+          <div class="owl-testimonials owl-carousel" style="position: relative; z-index: 5;">
+            <div class="item">
+              <p>“just think about TemplateMo if you need free CSS templates for your website”</p>
+                <h4>Catherine Walk</h4>
+                <span>CEO &amp; Founder</span>
+                <img src="assets/images/quote.png" alt="">
+            </div>
+            <div class="item">
+              <p>“think about our website first when you need free HTML templates for your website”</p>
+                <h4>David Martin</h4>
+                <span>CTO of Tech Company</span>
+                <img src="assets/images/quote.png" alt="">
+            </div>
+            <div class="item">
+              <p>“just think about our website wherever you need free templates for your website”</p>
+                <h4>Sophia Whity</h4>
+                <span>CEO and Co-Founder</span>
+                <img src="assets/images/quote.png" alt="">
+            </div>
+            <div class="item">
+              <p>“Praesent accumsan condimentum arcu, id porttitor est semper nec. Nunc diam lorem.”</p>
+                <h4>Helen Shiny</h4>
+                <span>Tech Officer</span>
+                <img src="assets/images/quote.png" alt="">
+            </div>
+            <div class="item">
+              <p>“Praesent accumsan condimentum arcu, id porttitor est semper nec. Nunc diam lorem.”</p>
+                <h4>George Soft</h4>
+                <span>Gadget Reviewer</span>
+                <img src="assets/images/quote.png" alt="">
+            </div>
+            <div class="item">
+              <p>“Praesent accumsan condimentum arcu, id porttitor est semper nec. Nunc diam lorem.”</p>
+                <h4>Andrew Hall</h4>
+                <span>Marketing Manager</span>
+                <img src="assets/images/quote.png" alt="">
+            </div>
+            <div class="item">
+              <p>“Praesent accumsan condimentum arcu, id porttitor est semper nec. Nunc diam lorem.”</p>
+                <h4>Maxi Power</h4>
+                <span>Fashion Designer</span>
+                <img src="assets/images/quote.png" alt="">
+            </div>
+            <div class="item">
+              <p>“Praesent accumsan condimentum arcu, id porttitor est semper nec. Nunc diam lorem.”</p>
+                <h4>Olivia Too</h4>
+                <span>Creative Designer</span>
+                <img src="assets/images/quote.png" alt="">
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>

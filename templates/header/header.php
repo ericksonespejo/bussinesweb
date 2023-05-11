0338782bd7e16b137cbbffd87d7d21f3947b4c94
Erickson <espejo.erickson@gmail.com>
2023-05-10 19:15:27 -0500

    añadido meta tag y ogg mas favicon

--- templates/header/header.php
+++ templates/header/header.php
@@ -5,11 +5,20 @@
 
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="description" content="">
-    <meta name="author" content="">
+    <meta name="description" content="Somos una agencia dedicada al desarrollo de páginas y sistemas web, también realizamos desarrollo de marca corporativa y diseño de logotipo">
+    <meta name="author" content="Erickson Espejo">
+    <meta name="keywords" content="desarrolo de paginas web, desarrollo de sistemas web, desarrollo de marca corporativa, marca corporativa, diseño web" >
+    <meta name="copyright"content="bussineswebsite.com">
+    <meta name="robots" content="index/">
+
+    <meta property=”og:url” content=”https://bussineswebsite.com” />
+    <meta property=”og:type” content=”article” />
+    <meta property="og:site_name" content="Marketing de Contenidos, desarrollo de páginas web" />
+
+    <link rel="shortcut icon" href="assets/images/favicon.png" type="image/x-icon">
     <link href="https://fonts.googleapis.com/css?family=Open+Sans:100,200,300,400,500,600,700,800,900" rel="stylesheet">
 
-    <title>EduWell - Education HTML5 Template</title>
+    <title>Desarrollo de páginas y sistemas web | Desarrollo de marca coorporativa</title>
 
     <!-- Bootstrap core CSS -->
     <link href="assets/css/bootstrap.min.css" rel="stylesheet">
@@ -19,7 +28,6 @@
     <link rel="stylesheet" href="assets/css/bussineswebsite-style.css">
     <link rel="stylesheet" href="assets/css/owl.css">
     <link rel="stylesheet" href="assets/css/lightbox.css">
-
   </head>
 
 <body>
